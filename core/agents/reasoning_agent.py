a1342fbc112515cc222f0d6e3dc829e18447c170
benjaminb <benbasseri@gmail.com>
2025-03-11 11:16:43 -0400

    working on test notebook

--- core/agents/reasoning_agent.py
+++ core/agents/reasoning_agent.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+from langchain.prompts import SystemMessagePromptTemplate
+from langchain_core.messages import SystemMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
@@ -11,47 +13,39 @@ MODEL = "mistral-nemo"
 TEMPERATURE = 0
 load_dotenv('.env', override=True)
 
-"""
-1. Define your State object
-"""
+
 class State(TypedDict):
     messages: Annotated[list[str], add_messages]
-    # additional keys and their value types...
+    claim: str
+    evidence: list[str]
 
-"""
-2. Define your Tools: fully annotated functions of any kind
-"""
-def multiply(a: int, b: int) -> int:
-    """Multiply `a` and `b`
+# Any tools needed?
+# tools_in_use = []
 
-    Args:
-        a: First number
-        b: Second number
-    """
-    return a * b
-
-tools_in_use = [multiply]
-
-""" 
-3. Instantiate your llm (Ollama or other) and bind the tools to it
-"""
 llm = ChatOllama(
     model=MODEL, 
     temperature=TEMPERATURE,
     base_url="http://host.docker.internal:11434", # if running in the studio
     ).bind_tools(tools_in_use)
 
+with open("prompts/reasoning_agent_system_prompt.txt", "r") as f:
+    prompt_text = f.read()
+
 """
 4. Define Nodes: functions taking state and returning state
 """
-def node_a(state: State) -> State:
-    # do something with state
-    print("=== node_a ===")
-    response = llm.invoke(state["messages"])
+def prompt_prep_node(state: State) -> State:
+    evidence_str = "\n".join([f"* {e}" for e in state["evidence"]])
+    state['evidence_str'] = evidence_str
+    return state
 
-    # Here we return an object following the State type;
-    # by annotating `add_message`, the new response will be appended
-    # to the messages list and assigned an id
+    
+def reasoning_node(state: State) -> State:
+    formatted_prompt = prompt_text.format(evidence=state["evidence_str"])
+
+    # Prepend the system prompt to the messages list
+    messages = [SystemMessage(content=formatted_prompt)] + state["messages"]
+    response = llm.invoke(messages)
     return {"messages": [response]}
 
 """
@@ -59,12 +53,12 @@ def node_a(state: State) -> State:
 """
 
 builder = StateGraph(State)
-builder.add_node("node_a", node_a)
-builder.add_node("tools", ToolNode(tools_in_use))
+builder.add_node("prompt_prep", prompt_prep_node)
+builder.add_node("reasoning", reasoning_node)
 
-builder.add_conditional_edges("node_a", tools_condition)
-builder.add_edge(START, "node_a")
-builder.add_edge("tools", END)
+builder.add_edge(START, "prompt_prep")
+builder.add_edge("prompt_prep", "reasoning")
+builder.add_edge("reasoning", END)
 graph = builder.compile()
 
 
