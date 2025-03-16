ddf4d9caabd5f9009b69072df2dbb9b162199966
Taufique Noorani <taufique@gmail.com>
2025-03-15 20:29:42 -0700

    adding django app - initial commit

---
+++ django/user_info/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import register, signin, home, search, logout_view, forgot_password_view
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('signup/', register, name='signup'),
+    path('signin/', signin, name='signin'),
+    path('search/', search, name='search'),
+    path('logout/', logout_view, name='logout'),
+    path('forgot/', forgot_password_view, name='forgot'),
+]
