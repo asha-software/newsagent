c023e424f0ff62e6c21202555952b7c63b3b4634
Shawn Joseph <shj006@g.harvard.edu>
2025-03-22 16:01:04 -0700

    Add initial WIP remote tool

---
+++ core/agents/tools/remote_tool.py
@@ -0,0 +1,7 @@
+from tool_base import tool
+
+class RemoteTool(tool):
+    def __init__(self, name: str, url: str, api_key: str):
+        super().__init__(name)
+        self.url = url
+        self.api_key = api_key
