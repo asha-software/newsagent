f7f0cb60da30497bdff2984876393ccb72fde2a5
banjopickin <yihualeng@gmail.com>
2025-03-23 10:15:33 -0700

    some progress

--- core/agents/tools/calculator.py
+++ core/agents/tools/calculator.py
@@ -1,8 +1,9 @@
 from typing import Any
-from tool_base import tool
+from .tool_base import tool
 import numbers
+from langchain_core.tools import tool
 
-
+@tool(description="This tool adds two numbers.")
 def add(a: numbers.Complex, b: numbers.Complex) -> Any:
     """Add `a` and `b`
 
@@ -12,7 +13,7 @@ def add(a: numbers.Complex, b: numbers.Complex) -> Any:
     """
     return a + b
 
-
+@tool(description="This tool devides two complex numbers.")
 def divide(a: numbers.Complex, b: numbers.Complex) -> Any:
     """Divide `a` and `b`
 
@@ -32,19 +33,22 @@ def integer_divide(a: int, b: int) -> int:
     """
     return a // b
 
-
+@tool(description="Resgister function add, devide and integer_devide detail function instruction, refer to the original function in this script")
 class Calculator(tool):
-
     def __init__(self):
         super().__init__("Calculator")
+        '''
+        resgister function add, devide and integer_devide
+        detail function instruction, refer to the original function in this script
+        '''
         self.register_tool_function(add)
         self.register_tool_function(divide)
         self.register_tool_function(integer_divide)
 
 
-if __name__ == "__main__":
-    c = Calculator()
-    print(c.use_tool("add", (1, 14)))
-    print(c.use_tool("divide", (1, 2)))
-    print(c.use_tool("integer_divide", (1, 2)))
-    print(c.use_tool("whoami", tuple()))
+# if __name__ == "__main__":
+#     c = Calculator()
+#     print(c.use_tool("add", (1, 14)))
+#     print(c.use_tool("divide", (1, 2)))
+#     print(c.use_tool("integer_divide", (1, 2)))
+#     print(c.use_tool("whoami", tuple()))
