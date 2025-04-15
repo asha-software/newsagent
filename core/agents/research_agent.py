0b9aeaf16192bd4b57b25804ecaea55dc5158bbf
benjaminb <benbasseri@gmail.com>
2025-04-15 10:54:37 -0400

    factors out nodes from create_agent() in research_agent.py so they can be individually tested, and making them more consistent with the other agents which also define the nodes at top level. Also renames wolfram_alpha tool to wolframalpha in the langchain @tool decorator, to make more consistent. Finally, this commit also cleans out some no longer needed path resolutions from create_agent. The only path that's resolved is to the parent dir, which is used to access the .env file and the prompt (file input). Module imports are handled by editable install

--- core/agents/research_agent.py
+++ core/agents/research_agent.py
@@ -9,28 +9,25 @@ import importlib
 import os
 from pathlib import Path
 
+from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, ToolMessage, SystemMessage
-from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from typing import Annotated, TypedDict, Callable
+from core.agents.tools.tool_registry import create_tool
 from core.agents.utils.llm_factory import get_chat_model
 from core.agents.utils.common_types import Evidence
 
-# Absolute path to this dir. For relative paths like prompts
+# Absolute path to this dir. For handling relative paths like to prompt file
 DIR = Path(__file__).parent.resolve()
 
-# Absolute path to repo root. This will be used to import builtin tools and Evidence from common_types
-ROOT_DIR = DIR.parent.parent
-
-# Path prefix for builtin tools
-PACKAGE_PREFIX = "core.agents.tools.builtins."
-
+#NOTE Assumes .env is in the parent directory
 load_dotenv(DIR.parent / '.env', override=True)
 assert "RESEARCH_AGENT_MODEL" in os.environ, "Please set the RESEARCH_AGENT_MODEL environment variable"
-PATH_TO_FILE = os.path.abspath(__file__)
 
+# Import prefix for builtin tools
+MODULE_PREFIX = "core.agents.tools.builtins."
 
 def import_builtin(module_name):
     """Dynamically imports a function from a module.
@@ -42,7 +39,7 @@ def import_builtin(module_name):
     Returns:
         The imported function, or None if the module or function is not found.
     """
-    import_path = PACKAGE_PREFIX + module_name
+    import_path = MODULE_PREFIX + module_name
     # Standard interface for builtin tool: each module has a function called tool_function
     function_name = 'tool_function'
     try:
@@ -73,8 +70,6 @@ def render_user_defined_tools(tool_kwargs: list[dict]) -> list[Callable]:
     if not tool_kwargs:
         return []
 
-    from core.agents.tools.tool_registry import create_tool
-
     tools = []
     for kwargs in tool_kwargs:
         try:
@@ -84,94 +79,100 @@ def render_user_defined_tools(tool_kwargs: list[dict]) -> list[Callable]:
             print(f"Error creating tool with kwargs {kwargs}:\n{e}")
     return tools
 
+"""
+Static graph components
+"""
+class State(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    claim: str
+    evidence: list[Evidence]
+
+with open(DIR / 'prompts/research_agent_system_prompt.txt', 'r') as f:
+    sys_msg = SystemMessage(content=f.read())
+
+def preprocessing(state: State):
+    """
+    Preprocesses state before sending to the assistant for tool routing.
+    Currently, this just extracts the claim from the state and sets it as a HumanMessage
+    following the SystemMessage
+    """
+    state['messages'] = [sys_msg, HumanMessage(content=state['claim'])]
+    return state
+
+def get_assistant_node(llm: BaseChatModel) -> Callable:
+    """
+    Given reference to LLM, returns an assistant node using that LLM
+    """
+    def assistant(state: State) -> State:
+        response = llm.invoke(state['messages'])
+        return {"messages": response}
+    
+    return assistant
+
+
+def postprocessing(state: State) -> State:
+    """
+    Scan the message history to extract tool calls and results into tuples:
+    (tool_name, tool_args, tool_result) for the 'evidence' list in the state
+
+    #TODO there's possibly a smarter way to do this by matching tool call IDs
+    """
+    evidence = []
+    for i in range(len(state['messages'])):
+        message = state['messages'][i]
+        if isinstance(message, AIMessage) and hasattr(message, 'tool_calls'):
+            for tool_call in message.tool_calls:
+                # Scan later messages for the corresponding ToolMessage
+                for j in range(i + 1, len(state['messages'])):
+                    next_message = state['messages'][j]
+                    if isinstance(next_message, ToolMessage) and next_message.tool_call_id == tool_call['id']:
+                        # Found the corresponding ToolMessage
+                        evidence_item = Evidence(
+                            name=tool_call['name'], args=tool_call['args'], result=next_message.content)
+                        evidence.append(evidence_item)
+                        break
+
+    return {'evidence': evidence}
 
 def create_agent(
         model: str,
-        builtin_tools: dict[str, str] = None,
+        builtin_tools: list[str] = None,
         user_tool_kwargs: list[dict] = None) -> StateGraph:
     """
     Build the research agent graph.
     Args:
         model (str): The model to use for the agent.
-        builtin_tools (dict[str, str]): A dictionary of builtin tools to use.
-            Keys are package names, which will be prepending with PACKAGE_PREFIX, 
+        builtin_tools (list[str]): A list of builtin tools to use identified by strings
+            The strings are module names, which will be prepending with PACKAGE_PREFIX, 
             values are lists of function names.
-            E.g. {'wikipedia': ['query']} will attempt to import PACKAGE_PREFIX + 'wikipedia' and
-            use the 'query' function from that module. (core.agents.tools.builtins.wikipedia.query)
-        user_tools (list[dict]): A list of user-defined tools to use.
+            E.g. 'wikipedia' will attempt to import PACKAGE_PREFIX + 'wikipedia'
+            (core.agents.tools.builtins.wikipedia)
+        user_tool_kwargs (list[dict]): A list of user-defined tools to use.
             each dict should be kwargs needed for tool_registry.create_tool
     Returns:
         StateGraph: The compiled state graph for the research agent.
     """
 
-    # Get builtin tools, filter out None values
+    # Assemble builtin and user-defined tools
     builtins = [tool for module in builtin_tools if (
         tool := import_builtin(module))]
-
-    # Handle user-defined tools
     user_defined_tools = render_user_defined_tools(
         tool_kwargs=user_tool_kwargs)
-
     tools = builtins + user_defined_tools
+
+    # Instantiate LLM-based objects for the agent (ChatModel, assistant node)
     if not model:
         model = os.getenv("RESEARCH_AGENT_MODEL")
-
     llm = get_chat_model(model_name=model).bind_tools(tools)
+    assistant = get_assistant_node(llm)
 
-    class State(TypedDict):
-        messages: Annotated[list[BaseMessage], add_messages]
-        claim: str
-        evidence: list[Evidence]
-
-    with open(DIR / 'prompts/research_agent_system_prompt.txt', 'r') as f:
-        sys_msg = SystemMessage(content=f.read())
-
-    def preprocessing(state: State):
-        """
-        Preprocesses state before sending to the assistant for tool routing.
-        Currently, this just extracts the claim from the state and sets it as a HumanMessage
-        following the SystemMessage
-        """
-        state['messages'] = [sys_msg, HumanMessage(content=state['claim'])]
-        return state
-
-    def assistant(state: State) -> State:
-        response = llm.invoke(state['messages'])
-        return {"messages": response}
-
-    def postprocessing(state: State) -> State:
-        """
-        Scan the message history to extract tool calls and results into tuples:
-        (tool_name, tool_args, tool_result) for the 'evidence' list in the state
-        """
-
-        evidence = []
-        for i in range(len(state['messages'])):
-            message = state['messages'][i]
-            if isinstance(message, AIMessage) and hasattr(message, 'tool_calls'):
-                for tool_call in message.tool_calls:
-                    # Scan later messages for the corresponding ToolMessage
-                    for j in range(i + 1, len(state['messages'])):
-                        next_message = state['messages'][j]
-                        if isinstance(next_message, ToolMessage) and next_message.tool_call_id == tool_call['id']:
-                            # Found the corresponding ToolMessage
-                            evidence_item = Evidence(
-                                name=tool_call['name'], args=tool_call['args'], result=next_message.content)
-                            evidence.append(evidence_item)
-                            break
-
-        return {'evidence': evidence}
-
-    # Graph
+    # Build graph
     builder = StateGraph(State)
-
-    # Define nodes: these do the work
     builder.add_node("preprocessing", preprocessing)
     builder.add_node("assistant", assistant)
     builder.add_node("tools", ToolNode(tools))
     builder.add_node("postprocessing", postprocessing)
 
-    # Define edges: these determine how the control flow moves
     builder.add_edge(START, "preprocessing")
     builder.add_edge("preprocessing", "assistant")
     builder.add_conditional_edges(
