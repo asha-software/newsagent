04e24b4ff7688a9cf3bfc8287963fb0faeb59919
shawnline <17132478+shawnline@users.noreply.github.com>
2025-04-27 23:29:16 +0000

    :art: Format Python code with psf/black

--- core/agents/agent_stub.py
+++ core/agents/agent_stub.py
@@ -9,18 +9,23 @@ from typing import Annotated, TypedDict
 
 MODEL = "mistral-nemo"
 TEMPERATURE = 0
-load_dotenv('.env', override=True)
+load_dotenv(".env", override=True)
 
 """
 1. Define your State object
 """
+
+
 class State(TypedDict):
     messages: Annotated[list[str], add_messages]
     # additional keys and their value types...
 
+
 """
 2. Define your Tools: fully annotated functions of any kind
 """
+
+
 def multiply(a: int, b: int) -> int:
     """Multiply `a` and `b`
 
@@ -30,20 +35,23 @@ def multiply(a: int, b: int) -> int:
     """
     return a * b
 
+
 tools_in_use = [multiply]
 
 """ 
 3. Instantiate your llm (Ollama or other) and bind the tools to it
 """
 llm = ChatOllama(
-    model=MODEL, 
+    model=MODEL,
     temperature=TEMPERATURE,
-    base_url="http://host.docker.internal:11434", # if running in the studio
-    ).bind_tools(tools_in_use)
+    base_url="http://host.docker.internal:11434",  # if running in the studio
+).bind_tools(tools_in_use)
 
 """
 4. Define Nodes: functions taking state and returning state
 """
+
+
 def node_a(state: State) -> State:
     # do something with state
     print("=== node_a ===")
@@ -53,6 +61,7 @@ def node_a(state: State) -> State:
     # by annotating `add_message`, the new response will be appended
     # to the messages list and assigned an id
     return {"messages": [response]}
+
 
 """
 5. Build the graph
@@ -66,5 +75,3 @@ builder.add_conditional_edges("node_a", tools_condition)
 builder.add_edge(START, "node_a")
 builder.add_edge("tools", END)
 graph = builder.compile()
-
-
